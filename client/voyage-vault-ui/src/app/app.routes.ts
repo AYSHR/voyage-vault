db6d9b962585acbacfbea43d4ebfa112c0942981
Ayushree Mishra <ayushree.mishra@brambles.com>
2025-12-30 22:28:28 +0530

    Removed client folder

--- client/voyage-vault-ui/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-  {
-    path: 'chat',
-    loadComponent: () => import('./components/chat/chat.component').then(m => m.ChatComponent),
-  },
-  { path: '', redirectTo: 'chat', pathMatch: 'full' }
-];
